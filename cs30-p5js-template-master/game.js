6ed089480eb51e554a0cb898566c3a3c6824ccee
AaronMenke <45469774+AaronMenke@users.noreply.github.com>
2020-03-02 15:17:51 -0600

    fixing wasd (S&D need work)

--- cs30-p5js-template-master/game.js
+++ cs30-p5js-template-master/game.js
@@ -5,23 +5,76 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let shyguy;
-let shyguyX;
-let shyguyY;
+let luigi;
+let luigiX;
+let luigiY;
+let luigiDX = 5;
+let luigiDY = 5;
+let movingUp = false;
+let movingDown = false;
+let movingLeft = false;
+let movingRight = false;
+
+
+//Creating Image
+function preload() {
+  luigi = loadImage("assets/luigi.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  luigiX = width/2;
+  luigiY = height/2;
 }
 
 function draw() {
   background(220);
-  
+  displayLuigi();
+  moveLuigi();
 }
-//Creating Image
-function preload() {
-  shyguy = loadImage("assets/shyguy.png");
+function keyPressed(){
+  if (key === "w"){
+    movingUp = true;
+  }
+  if (key === "a"){
+    movingLeft = true;
+  }
+  if (key === "s"){
+    movingDown = true;
+  }
+  if (key === "d"){
+    movingRight = true;
+  }
 }
 
-function displayshyguy(){
-  image(shyguy, shyguyX, shyguyY, 100, 100);
+function keyReleased(){
+  if (key === "w"){
+    movingUp = false;
+  }
+  if (key === "a"){
+    movingLeft = false;
+  }
+  if (key === "s"){
+    movingRight = false;
+  }
+  if (key === "d"){
+    movingDown = false;
+  }
 }
+function moveLuigi(){
+  if (movingUp){
+    luigiY -= luigiDY;
+  }
+  if (movingLeft){
+    luigiX -= luigiDX;
+  }
+  if (movingDown){
+    luigiY += luigiDY;
+  }
+  if (movingRight){
+    luigiX += luigiDX;
+  }
+}
+
+function displayLuigi(){
+  image(luigi, luigiX, luigiY, 100, 100);
