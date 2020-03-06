4e3bfc6bfcd029349a34765e29d924c0bc814937
AaronMenke <45469774+AaronMenke@users.noreply.github.com>
2020-03-06 15:16:30 -0600

    Tried to fix game but just ended up killing my mark

--- cs30-p5js-template-master/game.js
+++ cs30-p5js-template-master/game.js
@@ -1,107 +1,109 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+ //Project Title
+ //Your Name
+ //Date
 
-let luigi;
-let luigiX, luigiY;
-let luigiDX = 10;
-let luigiDY = 10;
-let movingUp = false;
-let movingDown = false;
-let movingLeft = false;
-let movingRight = false;
-let wallX, wallY;
-let wallW = 100;
-let wallH;
-let wallDX = 5;
+ //Extra for Experts:
+ //- describe what you did to take this project "above and beyond"
+
+ let luigi;
+ let luigiX, luigiY;
+ let luigiDX = 5;
+ let luigiDY = 5;
+ let movingUp = false;
+ let movingDown = false;
+ let movingLeft = false;
+ let movingRight = false;
+//  let wallX, wallY;
+//  let wallW = 100;
+//  let wallH;
+//  let wallDX = 5;
 
 
-//Creating Image
-function preload() {
-  luigi = loadImage("assets/luigi.png");
+ //Creating Image
+ function preload() {
+   luigi = loadImage("assets/luigi.png");
+ }
+
+ function setup() {
+   createCanvas(windowWidth, windowHeight);
+   luigiX = width/7;
+   luigiY = height - height/7;
+  //  wallX = 0;
+  //  wallH = height;
+ }
+
+ function draw() {
+   background(220);
+   displayLuigi();
+  //  displayWall();
+   moveLuigi();
+  //  hitbox();
+ }
+ function keyPressed(){
+   if (key === "w"){
+     movingUp = true;
+   }
+   if (key === "a"){
+     movingLeft = true;
+   }
+   if (key === "s"){
+     movingDown = true;
+   }
+   if (key === "d"){
+     movingRight = true;
+   }
+ }
+
+ function keyReleased(){
+   if (key === "w"){
+     movingUp = false;
+   }
+   if (key === "a"){
+     movingLeft = false;
+   }
+   if (key === "s"){
+     movingDown = false;
+   }
+   if (key === "d"){
+     movingRight = false;
+   }
+ }
+ function moveLuigi(){
+   if (movingUp){
+     luigiY -= luigiDY;
+   }
+   if (movingLeft){
+     luigiX -= luigiDX;
+   }
+   if (movingDown){
+     luigiY += luigiDY;
+   }
+   if (movingRight){
+     luigiX += luigiDX;
+   }
+ }
+
+ function displayLuigi(){
+   image(luigi, luigiX, luigiY, 100, 100);
+ }
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  luigiX = width/7;
-  luigiY = height - height/7;
-  wallX = 0;
-  wallH = height;
-}
+//  function moveWall() {
+//    if (wallX >= 0){
+//      wallY = random(height/2, height);
+//      wallX = width;
+//    }
+//    wallX -= wallDX;
+//  }
+//  function hitBox(){
+//    if (luigiY + 100 >= wallY) {
+//      if (luigiX + 100 >= wallX) {
+//        textSize(150);
+//        fill("blue")
+//        text("LOSE", width/8, height - height/2);
+//        wallDX = 5;
+//      }
+//    }
+//  }
 
-function draw() {
-  background(220);
-  displayLuigi();
-  displayWall();
-  moveLuigi();
-  hitbox();
-}
-function keyPressed(){
-  if (key === "w"){
-    movingUp = true;
-  }
-  if (key === "a"){
-    movingLeft = true;
-  }
-  if (key === "s"){
-    movingDown = true;
-  }
-  if (key === "d"){
-    movingRight = true;
-  }
-}
 
-function keyReleased(){
-  if (key === "w"){
-    movingUp = false;
-  }
-  if (key === "a"){
-    movingLeft = false;
-  }
-  if (key === "s"){
-    movingRight = false;
-  }
-  if (key === "d"){
-    movingDown = false;
-  }
-}
-function moveLuigi(){
-  if (movingUp){
-    luigiY -= luigiDY;
-  }
-  if (movingLeft){
-    luigiX -= luigiDX;
-  }
-  if (movingDown){
-    luigiY += luigiDY;
-  }
-  if (movingRight){
-    luigiX += luigiDX;
-  }
-}
-
-function displayLuigi(){
-  image(luigi, luigiX, luigiY, 100, 100);
-}
-
-function moveWall() {
-  if (wallX >= 0){
-    wallY = random(height/2, height);
-    wallX = width;
-  }
-  wallX -= wallDX;
-}
-function hitBox(){
-  if (luigiY + 100 >= wallY) {
-    if (luigiX + 100 >= wallX) {
-      textSize(150);
-      fill("blue")
-      text("LOSE", width/8, height - height/2);
-      wallDX = 5;
-    }
-  }
-}
-}
